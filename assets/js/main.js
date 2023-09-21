d69a0ecb3f5cdb14251768232c4dd1e2ad7061ea
MarlaRey <hejmarlenehej@gmail.com>
2023-09-21 12:17:03 +0200

    de sidste tilføjelser
    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,4 +112,54 @@ const startsWithS = animals.findIndex(animal => {
 });
 console.log(startsWithS);
 
-//The .reduce() Method
+//The .reduce() Method - dette forstår jeg ikke
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
+
+console.log(newSum);
+
+//Iterator Documentation
+const words = ["unique", "uncanny", "pique", "oxymoron", "guise"];
+
+// Something is missing in the method call below
+
+console.log(
+  words.some(() => {
+    return word.length < 6;
+  })
+);
+
+// Use filter to create a new array
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+// console.log(interestingWords.every((word) => { } ));
+
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
+// OR nums.some(num => num < 0);
