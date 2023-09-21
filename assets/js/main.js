ef478523010c73818785f2e2be72610c3b6c2d2c
MarlaRey <hejmarlenehej@gmail.com>
2023-09-21 11:24:19 +0200

    jeg er i gang med anden del af leksionen

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,89 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
   
   console.log(checkConsistentOutput(addTwo, 1));
   
+  //Intro til iterators
+  const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
+
+//forEach
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit =>
+console.log('I want to eat a '+fruit));
+
+//The .map() Method
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal =>
+  animal[0]
+);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number =>
+{return number / 100;
+});
+
+console.log(bigNumbers);
+console.log(smallNumbers);
+
+//The .filter() Method
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(number => {
+  return number<250;
+});
+console.log(randomNumbers);
+console.log(smallNumbers);
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7;
+});
+console.log(favoriteWords);
+console.log(longFavoriteWords);
+
+//The .findIndex() Method
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's';
+});
+console.log(startsWithS);
+
+//The .reduce() Method
